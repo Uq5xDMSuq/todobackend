357cc508d740ad9243fe50c42092ec60e9481562
Samer <sss@docker.home.internal>
2017-08-15 20:16:43 +1000

    Modified the scripts and docker files such that build artifacts are now generated

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,14 @@
 # Activate virtual environment
 . /appenv/bin/activate
 
-# Install application test requirements
-pip install -r requirements_test.txt
+# Download requirements to build cache. This tells pip to 
+# download all dependencies to the /build folder.
+pip download -d /build -r requirements_test.txt --no-input
+
+# Install application test requirements. --no-index tells
+# not to attempt to download external dependencies. Instead
+# get all dependencies from the /build folder
+pip install --no-index -f /build -r requirements_test.txt
 
 # Run test.sh arguments
 exec $@
